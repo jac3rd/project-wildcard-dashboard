a5f4b02a18c8082bba23d4bfd1d55fde22159260
boyd2a <boydandrewl3710@gmail.com>
2020-02-11 19:41:47 -0500

    Changed SQLite Database settings to PostgreSQL settings in DATABASE

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dashboard',
+        'USER': 'wildcard',
+        'PASSWORD': 'pass123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
