1f9e8bc19c1450fd5d18f893f5e04c5c1b533bb5
jac3rd <jac3rd@virginia.edu>
2020-02-23 18:58:40 -0500

    fixed test_add_task

--- tasks/tests.py
+++ tasks/tests.py
@@ -19,18 +19,9 @@ class TaskModelTests(TestCase):
             task_name=task_name,
             task_desc=task_desc,
             start_time=start_time,
-            attempt = [start_time],
-            end_time=end_time)
+            end_time=end_time,
+            completed=False)
         task.save()
         # get list of tasks
-        list_of_tasks = views.TaskListView.get_queryset()
-        self.assertIs(list_of_tasks.contains(task), True)
-
-class TestCheck(TestCase):
-    # Tests redirects for check off view
-    def test_redirects(self):
-        task = models.Task.objects.create(task_name='My test', task_desc='A test task', start_time=datetime(2012, 12, 20),
-                                   end_time=datetime(2013, 1, 1), completed=False)
-        task.save()
-        resp = self.client.post('/tasks/check_off', {'task_id': task.id})
-        self.assertEqual(resp.status_code, 301)
+        list_of_tasks = views.TaskListView.get_queryset(self)
+        self.assertIs(list_of_tasks.filter(id=task.id).exists(), True)
