9f3e1b8d268587e92d55f1b291659c6ed0e5f12d
jac3rd <jac3rd@virginia.edu>
2020-02-27 18:47:08 -0500

    removed the unexpected argument

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -122,7 +122,7 @@ DATABASES = {
     }   
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
 DATABASES['default']=db_from_env
 #DATABASES['default'] = dj_database_url.config()
 
