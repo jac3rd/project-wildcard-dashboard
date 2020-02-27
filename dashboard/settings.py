6337bd1e2e011daf13cdf28e9a80098cddfdddfb
jac3rd <jac3rd@virginia.edu>
2020-02-27 18:45:06 -0500

    changed use of update from dj_database_url

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -123,7 +123,7 @@ DATABASES = {
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default']=db_from_env
 #DATABASES['default'] = dj_database_url.config()
 
 # Password validation
