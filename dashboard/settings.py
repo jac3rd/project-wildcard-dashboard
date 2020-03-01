651858e9f16112db7a6647dd943a8bf3cb894eda
hcars <hcarscad@gmail.com>
2020-02-29 21:51:36 -0500

    Commented out sqllite option

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -107,14 +107,14 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'PORT': '5432',
-        # 'NAME': 'dashboard',
-        # 'USER': 'dashboard_user',
-        # 'PASSWORD': '123',
-        # 'HOST': 'localhost',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'PORT': '5432',
+        'NAME': 'dashboard',
+        'USER': 'dashboard_user',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
     }   
 }
 db_from_env = dj_database_url.config()
