0f515614a818fb424b85f5fd2cb4689892174cb7
hcars <42817130+hcars@users.noreply.github.com>
2020-03-01 15:26:30 -0500

    Update settings.py

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -120,6 +120,10 @@ DATABASES = {
         'HOST': 'localhost',
     }   
 }
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
