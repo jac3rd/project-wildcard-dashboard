bccb2c609d1e81d1fcaf7d0562c300c92a2d22fe
hcars <hcarscad@gmail.com>
2020-03-03 11:32:05 -0500

    Fixed the problem with url not being optional

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -113,14 +113,14 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'PORT': '5432',
-        'NAME': 'dashboard',
-        'USER': 'dashboard_user',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'PORT': '5432',
+        # 'NAME': 'dashboard',
+        # 'USER': 'dashboard_user',
+        # 'PASSWORD': '123',
+        # 'HOST': 'localhost',
     }   
 }
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views import generic
 import datetime
-
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def home(request):
+    print(User.objects.all())
     return render(request, 'home.html')
