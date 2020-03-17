3c60f9e0d761ed6b99b4bc0d128ae55e052826ec
jac3rd <46491149+jac3rd@users.noreply.github.com>
2020-03-17 19:31:00 -0400

    Tasks category (#21)
    * categories are done
    
    user has ability to, when adding a task, specify ONE category
    
    * tests and methods to add category
    
    * Update .gitignore
    
    * Update .gitignore
    
    * makemigrations
    
    * Update tests.py
    
    tests now create instantiate Task objects via create_task
    
    * Update tests.py
    
    minor comment changes

--- tasks/models.py
+++ tasks/models.py
@@ -6,25 +6,45 @@ from social_django import models as oauth_models
 
 
 class Task(models.Model):
-    # These are the date formats that we attempt to parse
-    attempt = ['%Y-%m-%d %H:%M:%S',  # '2006-10-25 14:30:59'
-               '%Y-%m-%d %H:%M',  # '2006-10-25 14:30'
-               '%Y-%m-%d',  # '2006-10-25'
-               '%m/%d/%Y %H:%M:%S',  # '10/25/2006 14:30:59'
-               '%m/%d/%Y %H:%M',  # '10/25/2006 14:30'
-               '%m/%d/%Y',  # '10/25/2006'
-               '%m/%d/%y %H:%M:%S',  # '10/25/06 14:30:59'
-               '%m/%d/%y %H:%M',  # '10/25/06 14:30'
-               '%m/%d/%y',
-               '%Y-%m-%dT%H:%M']  # '10/25/06'
-    # id = models.ForeignKey(oauth_models.uid, on_delete=models.CASCADE)
-    user = models.IntegerField(default=-1)
-    task_name = models.CharField(max_length=200)
-    task_desc = models.CharField(max_length=400)
-    start_time = models.DateTimeField('start time')
-    end_time = models.DateTimeField('end time')
-    completed = models.BooleanField('completed')
-    link = models.URLField(default="")
+	attempt = ['%Y-%m-%d %H:%M:%S',  # '2006-10-25 14:30:59'
+			'%Y-%m-%d %H:%M',  # '2006-10-25 14:30'
+			'%Y-%m-%d',  # '2006-10-25'
+			'%m/%d/%Y %H:%M:%S',  # '10/25/2006 14:30:59'
+			'%m/%d/%Y %H:%M',  # '10/25/2006 14:30'
+			'%m/%d/%Y',  # '10/25/2006'
+			'%m/%d/%y %H:%M:%S',  # '10/25/06 14:30:59'
+			'%m/%d/%y %H:%M',  # '10/25/06 14:30'
+			'%m/%d/%y',
+			'%Y-%m-%dT%H:%M'  # '10/25/06'
+	]
+	# id = models.ForeignKey(oauth_models.uid, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.task_name
+	user = models.IntegerField(default=-1)
+	task_name = models.CharField(max_length=200)
+	task_desc = models.CharField(max_length=400)
+	start_time = models.DateTimeField('start time')
+	end_time = models.DateTimeField('end time')
+	completed = models.BooleanField('completed')
+	link = models.URLField(default="")
+	HOMEWORK = 'hw'
+	CHORE = 'chore'
+	WORK = 'work'
+	ERRAND = 'errand'
+	LIFESTYLE = 'life'
+	CATEGORIES = [
+		(HOMEWORK, 'Homework'),
+		(CHORE, 'Chore'),
+		(WORK, 'Work'),
+		(ERRAND, 'Errand'),
+		(LIFESTYLE, 'Lifestyle'),
+	]
+	category = models.CharField(
+		null=True,
+		blank=True,
+		choices=CATEGORIES,
+		max_length=20,
+	)
+
+class Category(models.Model):
+	category_name = models.CharField(max_length=32)
+	user_id = models.IntegerField()
