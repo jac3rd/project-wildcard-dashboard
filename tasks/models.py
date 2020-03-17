2634096f07b81d6d9189e40ec81d7829303c1468
jac3rd <jac3rd@virginia.edu>
2020-03-17 17:16:13 -0400

    makemigrations

--- tasks/models.py
+++ tasks/models.py
@@ -45,9 +45,6 @@ class Task(models.Model):
 		max_length=20,
 	)
 
-	def __str__(self):
-
-
 class Category(models.Model):
 	category_name = models.CharField(max_length=32)
 	user_id = models.IntegerField()
