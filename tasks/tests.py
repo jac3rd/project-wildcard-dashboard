bc586a55ff271532eaaa30cd3bd5a9a885bab6f6
jac3rd <jac3rd@virginia.edu>
2020-03-17 18:32:27 -0400

    Update tests.py
    tests now create instantiate Task objects via create_task

--- tasks/tests.py
+++ tasks/tests.py
@@ -5,26 +5,31 @@ from django.urls import reverse
 from . import models, views
 from django.contrib.auth.models import User
 
+task_id = 0
+def create_task(user=0, task_name="generic test", task_desc="generic test description", start_time=timezone.now(), end_time=timezone.now() +  datetime.timedelta(days=1), completed=False, category=""):
+    global task_id
+    task = views.Task(
+        id=task_id,
+        user=user,
+        task_name=task_name,
+        task_desc=task_desc,
+        start_time=start_time,
+        end_time=end_time,
+        completed=completed,
+        category=category)
+    task.save()
+    task_id += 1
+    return task
 
 # Create your tests here.
 class TaskModelTests(TestCase):
-
+    
     # unit test for successfully adding generic task
     def test_add_task_once(self):
         # create task
         task_name = "test_add_task"
         task_desc = "test_add_task description"
-        start_time = timezone.now()
-        end_time = start_time + datetime.timedelta(days=3)
-        task = views.Task(
-            id=0,
-            user=1,
-            task_name=task_name,
-            task_desc=task_desc,
-            start_time=start_time,
-            end_time=end_time,
-            completed=False)
-        task.save()
+        task = create_task(user=1, task_name=task_name, task_desc=task_desc)
         # get list of tasks
         list_of_tasks = views.TaskListView.get_queryset(self)
         self.assertIs(list_of_tasks.filter(id=task.id).exists(), True)
@@ -37,15 +42,8 @@ class TaskModelTests(TestCase):
         task_desc = "test_add_task_start_after_end description"
         start_time = timezone.now()
         end_time = start_time - datetime.timedelta(days=3)
-        task = views.Task(
-            id=2,
-            user=3,
-            task_name=task_name,
-            task_desc=task_desc,
-            start_time=start_time,
-            end_time=end_time,
-            completed=False)
-        # try saving task with invalid date format
+        task = create_task(user=3, task_name=task_name, task_desc=task_desc, start_time=start_time, end_time=end_time)
+        # try saving task with invalid dates, but directly to database, not from view
         try:
             task.save()
         # should not throw an exception
@@ -60,16 +58,7 @@ class TaskModelTests(TestCase):
         # create task
         task_name = "test_check_off"
         task_desc = "test_check_off description"
-        start_time = timezone.now()
-        end_time = start_time + datetime.timedelta(days=3)
-        task = views.Task(
-            id=3,
-            user=4,
-            task_name=task_name,
-            task_desc=task_desc,
-            start_time=start_time,
-            end_time=end_time,
-            completed=False)
+        task = create_task(user=4, task_name=task_name, task_desc=task_desc)
         task.save()
         resp = self.client.post('/tasks/check_off', {'task_id': task.id})
         self.assertEqual(resp.status_code, 301)
@@ -78,16 +67,7 @@ class TaskModelTests(TestCase):
         # create task
         task_name = "test_checked"
         task_desc = "test_checked description"
-        start_time = timezone.now()
-        end_time = start_time + datetime.timedelta(days=3)
-        task = views.Task(
-            id=3,
-            user=2,
-            task_name=task_name,
-            task_desc=task_desc,
-            start_time=start_time,
-            end_time=end_time,
-            completed=False)
+        task = create_task(user=2, task_name=task_name, task_desc=task_desc)
         task.save()
         resp = self.client.post('/tasks/uncheck', {'task_id': task.id})
         self.assertEqual(resp.status_code, 301)
@@ -99,16 +79,7 @@ class TaskModelTests(TestCase):
         # create task
         task_name = "test_delete_task"
         task_desc = "test_delete_task description"
-        start_time = timezone.now()
-        end_time = start_time + datetime.timedelta(days=3)
-        task = views.Task(
-            id=4,
-            user=10,
-            task_name=task_name,
-            task_desc=task_desc,
-            start_time=start_time,
-            end_time=end_time,
-            completed=False)
+        task = create_task(user=10, task_name=task_name, task_desc=task_desc)
         task.save()
         self.client.post('/tasks/delete_task', {'task_id': task.id})
         list_of_tasks = views.TaskListView.get_queryset(self)
@@ -118,29 +89,18 @@ class TaskModelTests(TestCase):
         # create task
         task_name = "test_delete_task"
         task_desc = "test_delete_task description"
-        start_time = timezone.now()
-        end_time = start_time + datetime.timedelta(days=3)
-        task = views.Task(
-            id=5,
-            user=8,
-            task_name=task_name,
-            task_desc=task_desc,
-            start_time=start_time,
-            end_time=end_time,
-            completed=False)
+        task = create_task(user=8, task_name=task_name, task_desc=task_desc)
         task.save()
         resp = self.client.post('/tasks/delete_task',{'task_id':task.id})
         self.assertEqual(resp.status_code,302)
 
     def test_add_task_category(self):
         # create task
-        resp = self.client.post('/task/add_task', {
-            'task_name': 'test_add_task_category',
-            'task_desc': 'test_add_task_category description',
-            'start_time': timezone.now(),
-            'end_time': timezone.now() + datetime.timedelta(days=3),
-            'category': 'hw',
-        })
-        taskList = models.Task.objects.all
+        task_name = "test_add_task_category"
+        task_desc = "test_add_task_category description"
+        category = "Homework"
+        task = create_task(task_name=task_name, task_desc=task_desc, category=category)
+        task.save()
         # TEST NOT FINISHED
-        self.assertTrue(True)
+        list_of_tasks = views.TaskListView.get_queryset(self)
+        self.assertIs(list_of_tasks.get(id=task.id).category == category, True)
