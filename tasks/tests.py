78ef7cf2908f8ad94a3452ffaff088278594e04c
jac3rd <jac3rd@virginia.edu>
2020-03-20 18:35:16 -0400

    Update tests.py
    removed tracking of task_id, as django automatically provides a primary key
    
    commented short desc's of each test

--- tasks/tests.py
+++ tasks/tests.py
@@ -5,11 +5,8 @@ from django.urls import reverse
 from . import models, views
 from django.contrib.auth.models import User
 
-task_id = 0
 def create_task(user=0, task_name="generic test", task_desc="generic test description", start_time=timezone.now(), end_time=timezone.now() +  datetime.timedelta(days=1), completed=False, category=""):
-    global task_id
-    task = views.Task(
-        id=task_id,
+    task = models.Task(
         user=user,
         task_name=task_name,
         task_desc=task_desc,
@@ -18,7 +15,6 @@ def create_task(user=0, task_name="generic test", task_desc="generic test descri
         completed=completed,
         category=category)
     task.save()
-    task_id += 1
     return task
 
 # Create your tests here.
@@ -31,7 +27,7 @@ class TaskModelTests(TestCase):
         task_desc = "test_add_task description"
         task = create_task(user=1, task_name=task_name, task_desc=task_desc)
         # get list of tasks
-        list_of_tasks = views.TaskListView.get_queryset(self)
+        list_of_tasks = models.Task.objects
         self.assertIs(list_of_tasks.filter(id=task.id).exists(), True)
 
         # unit test for successfully adding generic task
@@ -50,7 +46,7 @@ class TaskModelTests(TestCase):
         except:
             self.assertTrue(False)
         # get list of tasks
-        list_of_tasks = views.TaskListView.get_queryset(self)
+        list_of_tasks = models.Task.objects
         self.assertIs(list_of_tasks.filter(id=task.id).exists(), True)
 
     # unit test for checking off task, marking a task as completed
@@ -63,6 +59,7 @@ class TaskModelTests(TestCase):
         resp = self.client.post('/tasks/check_off', {'task_id': task.id})
         self.assertEqual(resp.status_code, 301)
 
+    # unit test asserting that uncheck marks task as not completed
     def test_uncheck(self):
         # create task
         task_name = "test_checked"
@@ -71,10 +68,11 @@ class TaskModelTests(TestCase):
         task.save()
         resp = self.client.post('/tasks/uncheck', {'task_id': task.id})
         self.assertEqual(resp.status_code, 301)
-        list_of_tasks = views.TaskListView.get_queryset(self)
+        list_of_tasks = models.Task.objects
         # Check to make sure it is not set to completed
         self.assertIs(list_of_tasks.filter(id=task.id, completed=True).exists(), False)
 
+    # unit test asserting that delete_task removes task from database
     def test_delete_task(self):
         # create task
         task_name = "test_delete_task"
@@ -82,18 +80,20 @@ class TaskModelTests(TestCase):
         task = create_task(user=10, task_name=task_name, task_desc=task_desc)
         task.save()
         self.client.post('/tasks/delete_task', {'task_id': task.id})
-        list_of_tasks = views.TaskListView.get_queryset(self)
+        list_of_tasks = models.Task.objects
         self.assertFalse(list_of_tasks.filter(id=task.id).exists())
 
+    # unit test asserting that delete_task redirects the user
     def test_delete_task_redirect(self):
         # create task
-        task_name = "test_delete_task"
-        task_desc = "test_delete_task description"
+        task_name = "test_delete_task_redirect"
+        task_desc = "test_delete_task_redirect description"
         task = create_task(user=8, task_name=task_name, task_desc=task_desc)
         task.save()
         resp = self.client.post('/tasks/delete_task',{'task_id':task.id})
         self.assertEqual(resp.status_code,302)
 
+    # unit test asserting that add_task_category saves the category field to a task
     def test_add_task_category(self):
         # create task
         task_name = "test_add_task_category"
@@ -101,5 +101,5 @@ class TaskModelTests(TestCase):
         category = "Homework"
         task = create_task(task_name=task_name, task_desc=task_desc, category=category)
         task.save()
-        list_of_tasks = views.TaskListView.get_queryset(self)
+        list_of_tasks = models.Task.objects
         self.assertIs(list_of_tasks.get(id=task.id).category == category, True)
