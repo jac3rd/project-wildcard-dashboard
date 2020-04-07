7af4aad4291cfbb0745af98e0d5f961c2d9e86d5
jac3rd <jac3rd@virginia.edu>
2020-04-07 04:34:53 -0400

    calendar list items have due times

--- tasks/tests.py
+++ tasks/tests.py
@@ -482,7 +482,7 @@ class CalendarTests(TestCase):
         calendar = Calendar()
         curr_time = datetime.datetime.now()
         html = calendar.formatday(curr_time.day, models.Task.objects)
-        self.assertEqual(html, f"<td><span class='date'>{curr_time.day}</span><ul>  </ul></td>")
+        self.assertEqual(html, f'<td><span class=\'date\'>{curr_time.day}</span><ul class="list-group">  </ul></td>')
 
     # unit test to assert that formatday returns correct HTML when there are tasks for a given day
     def test_formatday_tasks_exist(self):
@@ -491,7 +491,7 @@ class CalendarTests(TestCase):
         task = create_task(end_time=curr_time)
         task.save()
         html = calendar.formatday(curr_time.day, models.Task.objects)
-        self.assertEqual(html, f'<td><span class=\'date\'>{curr_time.day}</span><ul> <li> {task.task_name} </li> </ul></td>')
+        self.assertEqual(html, f'<td><span class=\'date\'>{curr_time.day}</span><ul class="list-group"> <li class="list-group-item"> {task.task_name} - {task.end_time.time()} </li> </ul></td>')
 
     # unit test to assert that formatweek returns correct HTML when week is empty
     def test_formatweek_empty_week(self):
--- tasks/utils.py
+++ tasks/utils.py
@@ -10,12 +10,12 @@ class Calendar(HTMLCalendar):
         super(Calendar, self).__init__()
 
     def formatday(self, day, tasks):
-        tasks_per_day = tasks.filter(end_time__day=day)
+        tasks_per_day = tasks.filter(end_time__day=day).order_by('end_time')
         d = ''
         for task in tasks_per_day:
-            d += f'<li> {task.task_name} </li>'
+            d += f'<li class="list-group-item"> {task.task_name} - {task.end_time.time()} </li>'
         if day != 0:
-            return f"<td><span class='date'>{day}</span><ul> {d} </ul></td>"
+            return f'<td><span class=\'date\'>{day}</span><ul class="list-group"> {d} </ul></td>'
         return '<td></td>'
 
     def formatweek(self, theweek, tasks):
@@ -26,9 +26,10 @@ class Calendar(HTMLCalendar):
 
     def formatmonth(self, withyear=True):
         tasks = Task.objects.filter(end_time__year=self.year, end_time__month=self.month)
-        cal = f'<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
+        cal = f'<table class="calendar table table-hover table-bordered table-striped"><tbody>\n'
         cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
         cal += f'{self.formatweekheader()}\n'
         for week in self.monthdays2calendar(self.year, self.month):
             cal += f'{self.formatweek(week, tasks)}\n'
+        cal += f'</tbody></table>'
         return cal
